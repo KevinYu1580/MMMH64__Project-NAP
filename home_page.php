eed9ab47aca096768fbff9de4472650dff9e7548
KevinYu1580 <kevin1580@gmail.com>
2022-09-05 22:14:54 +0800

    0905 01

---
+++ home_page.php
@@ -0,0 +1,14 @@
+<?php
+$pageName = 'Home'; // 頁面名稱
+?>
+<?php include __DIR__. '/parts/html-head.php'; ?>
+<?php include __DIR__. '/parts/navbar.php'; ?>
+<div class="container">
+    <h2>
+        Hello
+    </h2>
+
+</div>
+<?php include __DIR__. '/parts/scripts.php'; ?>
+
+<?php include __DIR__. '/parts/html-footer.php'; ?>
---
+++ parts/scriptes.php
@@ -0,0 +1,5 @@
+<!-- jQuery 3.6.0 -->
+<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+<!-- bootstrap 5 的JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" ></script>
+
