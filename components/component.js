228b3b40c2f47205625efe027aec3c19ff8c0664
Shen-Wan Yu <kevin1580@gmail.com>
2022-09-07 11:58:30 +0800

    0907-1158-k
    新增 兩格switch

--- components/component.js
+++ components/component.js
@@ -8,3 +8,19 @@ $('.napBtn_likeBtn').click(function(){
   $(this).toggleClass('likeBtn_activated');
   $('.napBtn_likeBtn #white').toggle();
 })
+
+// ----------switch---------
+// --兩格
+const backmask = $('.napSwitch_two .backmask')
+$('.napSwitch_two .num2').click(function(){
+  $(backmask).css('left', '50%');
+})
+$('.napSwitch_two .num1').click(function(){
+  $(backmask).css('left', '0%');
+})
+
+// --三格
+
+
+
+
