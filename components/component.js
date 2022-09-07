dcf0e03c5ad50a7ca75b3db5b144290cd1159f25
Shen-Wan Yu <kevin1580@gmail.com>
2022-09-07 11:07:26 +0800

    0907-1107-K

--- components/component.js
+++ components/component.js
@@ -2,3 +2,9 @@
 $(".topNav_mobile .menu").click(function () {
   $(".nap_menu ").toggleClass("nap_menu_open");
 });
+
+// ----------like_Btn---------
+$('.napBtn_likeBtn').click(function(){
+  $(this).toggleClass('likeBtn_activated');
+  $('.napBtn_likeBtn #white').toggle();
+})
