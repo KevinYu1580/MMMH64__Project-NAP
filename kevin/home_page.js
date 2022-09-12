e8f1566f9a343c56d885879a588785a0449bd5c2
KevinYu1580 <kevin1580@gmail.com>
2022-09-13 02:13:34 +0800

    0913-0213-k

--- kevin/home_page.js
+++ kevin/home_page.js
@@ -1,4 +1,4 @@
-// 進場卡片特效
+// ----首頁aniCover 視覺特效----
 $(window).scroll(function(){
     const window_top = $(window).scrollTop()
     const first_page = $('.first_page')
@@ -7,35 +7,67 @@ $(window).scroll(function(){
 
     let heightNum = '300vh';
 
-    // aniCover位移控制
-    if(first_page_top - window_top < 0){
+
+    // 控制aniCover遮罩的大小
+
+    if(window_top > screen.availHeight / 3 ){
+        $('.aniCover_mask').css('-webkit-mask-size', 'auto 180vh')
+        if(window_top > (screen.availHeight / 3  * 2) ){
+            $('.aniCover_mask').css('-webkit-mask-size', 'auto 300vh')
+            if(window_top > screen.availHeight ){
+                $('.aniCover_mask').css('-webkit-mask-size', 'auto 800vh')
+            }}}
+    else{
+        $('.aniCover_mask').css('-webkit-mask-size', 'auto 80vh')}
+
+    // 控制aniCover遮罩內文字方塊opacity & 文字顏色
+    if(window_top > (screen.availHeight * 1)){
+        $('.aniCover_mask .textWrap > span span').css('color', 'var(--primaryColor_tint)')
+    }
+    else{
+        $('.aniCover_mask .textWrap > span span').css('color', 'var(--white)')
+    }
+
+    if(window_top > (screen.availHeight * 2)){
+        $('.aniCover_mask .textWrap').fadeOut(600)
+    }
+    else{
+        $('.aniCover_mask .textWrap').fadeIn(600)
+    }
+
+    // 控制aniCover遮罩到達firstPage時的寬度
+        // aniCover位移控制
+    if(first_page_top - window_top < (screen.height /5)){
         aniCover.css({
             'position': 'absolute',
-            'top':`${heightNum}`
+            'top':`300vh`,
+            'opacity': '0',
+            
         })
-        // first_page.css('margin-top', '0')
-    }
+        }
     else {
         aniCover.css({
             'position': 'fixed',
-            'top':`0`
+            'top':`0`,
+            'width':'100%',
+            'opacity': '1',
+        })
+        }
+    if(first_page_top - window_top < (screen.availHeight /99)){
+        aniCover.css({
+            'width':'55%',
+            'opacity': '1',
+            'transition':'1s',
+        })
+        }
+    else {
+        aniCover.css({
+            'width':'100%',
+            'transition':'0s',
+            
         })
     }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -88,7 +120,7 @@ $(window).scroll(function(){
 
 
 // --------napSteps_pc 卡片翻轉--------
-
+// 進場卡片特效
 // 左向右滑入效果
 const card1_pc = $('.napSteps .wrap_pc .cardWrap #num1')
 const card2_pc = $('.napSteps .wrap_pc .cardWrap #num2')
