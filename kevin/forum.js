b7a95dbecdcfdd20d2a1b5b6d483163a156c1dc6
KevinYu1580 <kevin1580@gmail.com>
2022-09-14 01:29:59 +0800

    0914-0129-k

--- kevin/forum.js
+++ kevin/forum.js
@@ -7,3 +7,17 @@ $('.selector_wrap .secSelec #num2').click(function(){
     $(this).css('color', 'var(--secondaryColor_default)');
     $(this).prev().prev().css('color', 'var(--subColor_text)');
 })
+
+
+
+// 貼文卡片 以迴圈新增卡片數量
+function runCardLoop (){
+    let comtCardHtml =  document.getElementById('comtCard')
+
+    for (i=0; i<9; i++){
+        $('.comtCard_wrap').append($(comtCardHtml).prop('outerHTML'))
+    }
+}
+runCardLoop(); 
+
+
