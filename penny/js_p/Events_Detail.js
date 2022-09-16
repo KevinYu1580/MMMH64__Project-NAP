8dfba7ebf3162d8ed60c049e8cdceb978cc06d47
pennypan0111 <minipan0111@gmail.com>
2022-09-16 17:50:12 +0800

    0916-1750-p

--- penny/js_p/Events_Detail.js
+++ penny/js_p/Events_Detail.js
@@ -46,6 +46,10 @@ $(window).scroll(function(){
 $('.silder-dot-pc .dot1').click(function(){
     $('.dot').removeClass('default');
     $(this).addClass('default');
+    $('.pic1').removeClass('slide-active slide-prev slide-next');
+    $('.pic1').addClass('slide-active');
+    $('.pic2').addClass('slide-prev');
+    $('.pic1').removeClass('slide-next');
     $('.know-accompany-textbox h3').text('01 相見說明會');
     $('.know-accompany-textbox p:nth-of-type(2)').text('與您尬意的浪浪相見，然後透過N.A.P.的說明，讓您了解領養流浪動物的各種須知，並且說明本次兩天一夜行程及注意事項。');
 });
@@ -54,6 +58,8 @@ $('.silder-dot-pc .dot1').click(function(){
 $('.silder-dot-pc .dot2').click(function(){
     $('.dot').removeClass('default');
     $(this).addClass('default');
+    $('.pic2').removeClass('slide-active slide-prev slide-next');
+    $('.pic2').addClass('slide-active');
     $('.pic1').addClass('slide-prev');
     $('.pic2').removeClass('slide-next');
     $('.pic3').addClass('slide-next');
@@ -65,6 +71,8 @@ $('.silder-dot-pc .dot2').click(function(){
 $('.silder-dot-pc .dot3').click(function(){
     $('.dot').removeClass('default');
     $(this).addClass('default');
+    $('.pic3').removeClass('slide-active slide-prev slide-next');
+    $('.pic3').addClass('slide-active');
     $('.pic2').addClass('slide-prev');
     $('.pic3').removeClass('slide-next');
     $('.pic4').addClass('slide-next');
@@ -76,6 +84,7 @@ $('.silder-dot-pc .dot3').click(function(){
 $('.silder-dot-pc .dot4').click(function(){
     $('.dot').removeClass('default');
     $(this).addClass('default');
+    $('.pic4').removeClass('slide-active slide-prev slide-next');
     $('.pic3').addClass('slide-prev');
     $('.pic4').removeClass('slide-next');
     $('.pic5').addClass('slide-next');
@@ -87,6 +96,7 @@ $('.silder-dot-pc .dot4').click(function(){
 $('.silder-dot-pc .dot5').click(function(){
     $('.dot').removeClass('default');
     $(this).addClass('default');
+    $('.pic5').removeClass('slide-active slide-prev slide-next');
     $('.pic4').addClass('slide-prev');
     $('.pic5').removeClass('slide-next');
     $('.pic6').addClass('slide-next');
@@ -98,6 +108,7 @@ $('.silder-dot-pc .dot5').click(function(){
 $('.silder-dot-pc .dot6').click(function(){
     $('.dot').removeClass('default');
     $(this).addClass('default');
+    $('.pic6').removeClass('slide-active slide-prev slide-next');
     $('.pic5').addClass('slide-prev');
     $('.pic6').removeClass('slide-next');
     $('.pic7').addClass('slide-next');
@@ -109,6 +120,7 @@ $('.silder-dot-pc .dot6').click(function(){
 $('.silder-dot-pc .dot7').click(function(){
     $('.dot').removeClass('default');
     $(this).addClass('default');
+    $('.pic7').removeClass('slide-active slide-prev slide-next');
     $('.pic6').addClass('slide-prev');
     $('.pic7').removeClass('slide-next');
     $('.pic8').addClass('slide-next');
@@ -120,6 +132,7 @@ $('.silder-dot-pc .dot7').click(function(){
 $('.silder-dot-pc .dot8').click(function(){
     $('.dot').removeClass('default');
     $(this).addClass('default');
+    $('.pic8').removeClass('slide-active slide-prev slide-next');
     $('.pic7').addClass('slide-prev');
     $('.pic8').removeClass('slide-next');
     $('.pic9').addClass('slide-next');
@@ -131,6 +144,7 @@ $('.silder-dot-pc .dot8').click(function(){
 $('.silder-dot-pc .dot9').click(function(){
     $('.dot').removeClass('default');
     $(this).addClass('default');
+    $('.pic9').removeClass('slide-active slide-prev slide-next');
     $('.pic8').addClass('slide-prev');
     $('.pic9').removeClass('slide-next');
     $('.pic10').addClass('slide-next');
@@ -142,6 +156,7 @@ $('.silder-dot-pc .dot9').click(function(){
 $('.silder-dot-pc .dot10').click(function(){
     $('.dot').removeClass('default');
     $(this).addClass('default');
+    $('.pic10').removeClass('slide-active slide-prev slide-next');
     $('.pic9').addClass('slide-prev');
     $('.pic10').removeClass('slide-next');
     $('.pic9').addClass('slide-prev');
@@ -166,19 +181,20 @@ move.addEventListener("wheel", (event) => {
     move.scrollLeft += event.deltaY;
 });
 
+
 // 手機 | 了解陪伴-(換圖時，文字跟頁碼顯示點點會跟著變動)
 $('.know-accompany-imgbox').scroll(function(){
     // console.log('left:', $('.know-accompany-imgbox').scrollLeft());
     // console.log('width', $('.mb-train li').outerWidth(true));
     // console.log('top', $('.mb-train li').offset().top);
 
-    if($('.know-accompany-imgbox').scrollLeft() < 284){
+    if($('.know-accompany-imgbox').scrollLeft() < 270){
         $('.know-accompany-textbox h3').text('01 相見說明會');
         $('.know-accompany-textbox p:nth-of-type(2)').text('與您尬意的浪浪相見，然後透過N.A.P.的說明，讓您了解領養流浪動物的各種須知，並且說明本次兩天一夜行程及注意事項。');
     }
 
     // 從 1 → 2 | 2 → 1
-    if($('.know-accompany-imgbox').scrollLeft() >= 284 && $('.know-accompany-imgbox').scrollLeft() <= 568){
+    if($('.know-accompany-imgbox').scrollLeft() >= 270 && $('.know-accompany-imgbox').scrollLeft() <= 540){
         $('.dot2').css('backgroundColor', 'var(--primaryColor_dark)');
         $('.dot').removeClass('silder-dot-mb default')
         $('.know-accompany-textbox h3').text('02 動手做鮮食');
@@ -190,7 +206,7 @@ $('.know-accompany-imgbox').scroll(function(){
     }
 
     // 從 2 → 3 | 3 → 2
-    if($('.know-accompany-imgbox').scrollLeft() >= 568 && $('.know-accompany-imgbox').scrollLeft() <= 852){
+    if($('.know-accompany-imgbox').scrollLeft() >= 540 && $('.know-accompany-imgbox').scrollLeft() <= 810){
         $('.dot').removeClass('silder-dot-mb default')
         $('.dot3').css('backgroundColor', 'var(--primaryColor_dark)');
         $('.know-accompany-textbox h3').text('03 歡樂瘋戲水');
@@ -201,7 +217,7 @@ $('.know-accompany-imgbox').scroll(function(){
     }
 
     // 從 3 → 4 | 4 → 3
-    if($('.know-accompany-imgbox').scrollLeft() >= 852 && $('.know-accompany-imgbox').scrollLeft() <= 1136){
+    if($('.know-accompany-imgbox').scrollLeft() >= 810 && $('.know-accompany-imgbox').scrollLeft() <= 1080){
         $('.dot').removeClass('silder-dot-mb default')
         $('.dot4').css('backgroundColor', 'var(--primaryColor_dark)');
         $('.know-accompany-textbox h3').text('04 浪浪講學堂');
@@ -212,7 +228,7 @@ $('.know-accompany-imgbox').scroll(function(){
     }
 
     // 從 4 → 5 | 5 → 4
-    if($('.know-accompany-imgbox').scrollLeft() >= 1136 && $('.know-accompany-imgbox').scrollLeft() <= 1420){
+    if($('.know-accompany-imgbox').scrollLeft() >= 1080 && $('.know-accompany-imgbox').scrollLeft() <= 1350){
         $('.dot').removeClass('silder-dot-mb default')
         $('.dot5').css('backgroundColor', 'var(--primaryColor_dark)');
         $('.know-accompany-textbox h3').text('05 一起說晚安');
@@ -223,7 +239,7 @@ $('.know-accompany-imgbox').scroll(function(){
     }
     
     // 從 5 → 6 | 6 → 5
-    if($('.know-accompany-imgbox').scrollLeft() >= 1420 && $('.know-accompany-imgbox').scrollLeft() <= 1704){
+    if($('.know-accompany-imgbox').scrollLeft() >= 1350 && $('.know-accompany-imgbox').scrollLeft() <= 1620){
         $('.dot').removeClass('silder-dot-mb default')
         $('.dot6').css('backgroundColor', 'var(--primaryColor_dark)');
         $('.know-accompany-textbox h3').text('06 誰是小吃貨');
@@ -234,7 +250,7 @@ $('.know-accompany-imgbox').scroll(function(){
     }
     
     // 從 6 → 7 | 7 → 6
-    if($('.know-accompany-imgbox').scrollLeft() >= 1704 && $('.know-accompany-imgbox').scrollLeft() <= 1988){
+    if($('.know-accompany-imgbox').scrollLeft() >= 1620 && $('.know-accompany-imgbox').scrollLeft() <= 1890){
         $('.dot').removeClass('silder-dot-mb default')
         $('.dot7').css('backgroundColor', 'var(--primaryColor_dark)');
         $('.know-accompany-textbox h3').text('07 路跑大作戰');
@@ -245,7 +261,7 @@ $('.know-accompany-imgbox').scroll(function(){
     } 
 
     // 從 7 → 8 | 8 → 7
-    if($('.know-accompany-imgbox').scrollLeft() >= 1988 && $('.know-accompany-imgbox').scrollLeft() <= 2272){
+    if($('.know-accompany-imgbox').scrollLeft() >= 1890 && $('.know-accompany-imgbox').scrollLeft() <= 2160){
         $('.dot').removeClass('silder-dot-mb default')
         $('.dot8').css('backgroundColor', 'var(--primaryColor_dark)');
         $('.know-accompany-textbox h3').text('08 放電跑一跑');
@@ -256,7 +272,7 @@ $('.know-accompany-imgbox').scroll(function(){
     }
     
     // 從 8 → 9 | 9 → 8
-    if($('.know-accompany-imgbox').scrollLeft() >= 2272 && $('.know-accompany-imgbox').scrollLeft() <= 2556){
+    if($('.know-accompany-imgbox').scrollLeft() >= 2160 && $('.know-accompany-imgbox').scrollLeft() <= 2430){
         $('.dot').removeClass('silder-dot-mb default')
         $('.dot9').css('backgroundColor', 'var(--primaryColor_dark)');
         $('.know-accompany-textbox h3').text('09 幫浪浪洗澡');
@@ -267,7 +283,7 @@ $('.know-accompany-imgbox').scroll(function(){
     } 
 
     // 從 9 → 10 | 10 → 9
-    if($('.know-accompany-imgbox').scrollLeft() >= 2556){
+    if($('.know-accompany-imgbox').scrollLeft() >= 2430){
         $('.dot').removeClass('silder-dot-mb default')
         $('.dot10').css('backgroundColor', 'var(--primaryColor_dark)');
         $('.know-accompany-textbox h3').text('10 合照留紀念');
