0b61bf8d27531667b9b3e44d1b954fb9bdd7cefe
nora10069 <hweng028@gmail.com>
2022-09-18 11:49:38 +0800

    0918-1149-n

---
+++ nora/parts/connect_db.php
@@ -0,0 +1,23 @@
+<?php
+
+$db_host = 'localhost';
+$db_user = 'nora';
+$db_pass = 'nap2321';
+$db_name = 'nnn_57';
+
+$dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8"; // data source name
+
+$pdo_options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try{
+    $pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
+} catch(PDOException $ex) {
+    echo "Exceprion: ". $ex->getMessage();
+}
+
+if(! isset($_SESSION)){
+    session_start();
+}
