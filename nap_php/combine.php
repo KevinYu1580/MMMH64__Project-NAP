42d48349c179cbb195da2499b78eabee9126fce6
chiyiniyihc <chiyin0209@gmail.com>
2022-09-19 12:12:39 +0800

    0919-1212-cy

---
+++ nap_php/combine.php
@@ -0,0 +1,29 @@
+<?php
+
+// require __DIR__ .'./parts/connect_db.php';
+
+$pageName = ' 套裝活動報名'; //頁面名稱
+
+?>
+
+<?php include __DIR__ . '/parts/html-head.php'; ?>
+
+
+
+<!-- <link rel="stylesheet" href="/bootstrap-4.2.1-dist/css/bootstrap.css"> -->
+<link rel="stylesheet" href="/bootstrap-5.1.1-dist/css/bootstrap.css">
+
+</head>
+<?php include __DIR__ . '/parts/navbar.php'; ?>
+
+
+<body>
+
+<h4>combine檔案</h4>
+
+
+
+<?php include __DIR__ . '/parts/nap-footer.php'; ?>
+<?php include __DIR__ . '/parts/scripts.php'; ?>
+
+<?php include __DIR__ . '/parts/html-foot.php'; ?>
--- nap_php/parts/html-head.php
+++ nap_php/parts/html-head.php
@@ -16,7 +16,6 @@ if(! isset($title)){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/reset/reset.css">
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
-    <link rel="stylesheet" href="./bootstrap-5.1.1-dist/css/bootstrap.css">
-    <title><?= $title ?></title>
+    <link rel="stylesheet" href="/nap_css/component_mobile1.css">
+    
