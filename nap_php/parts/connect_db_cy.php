cf96df0bf691fc4bb6d4eacb845346a8b847ce64
chiyiniyihc <chiyin0209@gmail.com>
2022-09-19 18:15:25 +0800

    0919-1815-cy

---
+++ nap_php/parts/connect_db_cy.php
@@ -0,0 +1,23 @@
+<?php
+
+$db_host = 'localhost';
+$db_name = 'test-enroll';
+$db_user = 'chiyin_niyihc';
+$db_pass = 'Admin_chiyin0209';
+
+$dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8"; // data source name
+
+$pdo_options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try{
+    $pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
+} catch(PDOException $ex) {
+    echo "Exceprion: ". $ex->getMessage();
+}
+
+if(! isset($_SESSION)){
+    session_start();
+}
