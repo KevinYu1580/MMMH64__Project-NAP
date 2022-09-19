7b04e3630c30a44a3d44b217534b62ccfe414e5b
chiyiniyihc <chiyin0209@gmail.com>
2022-09-19 16:40:40 +0800

    0919-1640-cy
    php-parts

---
+++ nap_php/nap_js/event-enroll-data1.js
@@ -0,0 +1,112 @@
+var amount = 0;
+CheckAmount();
+
+
+$(document).ready(function () {
+    $(document).on('click', '.add-member-btn', function () {
+        if (amount < 4) {
+            $('.enroll-content-null').hide();
+            $('#enroll-area').prepend('<div class="enroll-content-form">\
+                                <div class="delete-btn">\
+                                    <a class="delete" href="javascript:" type="button">\
+                                        <img src="./img/component/icon/xmark.svg" alt="">\
+                                    </a>\
+                                </div>\
+                                <div class="count-num"></div>\
+                                <div class="enroll-other">\
+                                    <div class="mb-3 form-check">\
+                                        <input type="checkbox" class="form-check-input" id="add-member-data" name="add-member-data">\
+                                        <label class="form-check-label" for="exampleCheck1">加入會員資料（會員為活動參與者）</label>\
+                                    </div>\
+                                    <div class="mb-3">\
+                                        <label for="name" class="form-label">姓名</label>\
+                                        <input type="text" class="form-control" id="name" name="name[]">\
+                                    </div>\
+                                    <div class="mb-3">\
+                                        <label for="phone" class="form-label">手機</label>\
+                                        <input type="text" class="form-control" id="mobile" name="mobile[]" maxlength="10">\
+                                    </div>\
+                                    <div class="mb-3">\
+                                        <label for="birthdate" class="form-label">生日</label>\
+                                        <input type="date" class="form-control" id="birthdate" name="birthdate[]">\
+                                    </div>\
+                                    <div class="mb-3">\
+                                        <label for="id-num" class="form-label">身分證字號</label>\
+                                        <input type="text" class="form-control" id="id-num" name="id-num[]" maxlength="10">\
+                                    </div>\
+                                    <div class="mb-3">\
+                                        <label for="id-num" class="form-label">飲食習慣</label>\
+                                        <fieldset>\
+                                            <div class="mb-3">\
+                                                <div class="form-check form-check-inline">\
+                                                    <input class="form-check-input" type="radio"\
+                                                        id="inlineRadio1" name="eating-habit[]" value="葷">\
+                                                    <label class="form-check-label" for="inlineRadio1">葷</label>\
+                                                </div>\
+                                                <div class="form-check form-check-inline">\
+                                                    <input class="form-check-input" type="radio"\
+                                                        id="inlineRadio2" name="eating-habit[]"\
+                                                        value="全素">\
+                                                    <label class="form-check-label" for="inlineRadio2">全素</label>\
+                                                </div>\
+                                                <div class="form-check form-check-inline">\
+                                                    <input class="form-check-input" type="radio"\
+                                                        id="inlineRadio3" name="eating-habit[]" value="奶蛋素">\
+                                                    <label class="form-check-label" for="inlineRadio3">奶蛋素</label>\
+                                                </div>\
+                                            </div>\
+                                        </fieldset>\
+                                    </div>\
+                                </div>\
+                            </div>');
+            amount++;
+        }
+
+        // 呼叫判斷數量function
+        CheckAmount();
+        var eventPrice = 2999;
+        var enrollNum = (amount);
+        var total = eventPrice * enrollNum;
+        $('.total-price').html(total);
+        $('.enroll-num').html(amount);
+    });
+
+    $(document).on('click', '.delete-btn', function () {
+        $(this).closest('.enroll-content-form').remove();
+        amount--;
+        if (amount < 1) {
+            $('.enroll-content-null').show();
+        };
+
+        // 呼叫判斷數量function
+        CheckAmount();
+
+        var eventPrice = 2999;
+        var enrollNum = (amount);
+        var total = eventPrice * enrollNum;
+
+        // 新的值取代舊得值
+        $('.enroll-num').html(function (index, oldContent) {
+            $('.enroll-num').html(amount);
+        });
+        $('.total-price').html(function (index, oldContent) {
+            $('.total-price').html(total);
+        });
+
+    });
+
+
+});
+// 判斷amount數量，去顯示disable狀態
+function CheckAmount() {
+    if (amount > 3) {
+        $('.add-member-btn').addClass('disabled');
+    } else {
+        $('.add-member-btn').removeClass('disabled')
+    };
+    if (amount < 1) {
+        $('.enroll-card-btn').addClass('disabled');
+    } else {
+        $('.enroll-card-btn').removeClass('disabled')
+    };
+};
--- nap_php/parts/html-head.php
+++ nap_php/parts/html-head.php
@@ -16,6 +16,4 @@ if(! isset($title)){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./nap_css/reset.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
-    
     
