7bab3e0ad93ce4119c6a2cfde9c35dcd312a63ef
chiyiniyihc <chiyin0209@gmail.com>
2022-09-20 13:27:32 +0800

    0920-1327-cy

--- nap_php/event-enroll-data.php
+++ nap_php/event-enroll-data.php
@@ -94,8 +94,8 @@ $pageName = '套裝活動報名'; // 頁面名稱
                 </div>
 
 
-                <!-- <form action="getEnrollData.php" method="POST" id="enroll-list">
-                    <div class="enroll-content-form">
+                <form action="getEnrollData.php" method="POST" id="enroll-list">
+                    <!-- <div class="enroll-content-form">
                         <div class="delete-btn">
                             <a class="delete" href="javascript:" type="button">
                                 <img src="./img/component/icon/xmark.svg" alt="">
@@ -144,7 +144,7 @@ $pageName = '套裝活動報名'; // 頁面名稱
                             </div>
                         </div>
                     </div> -->
-                    
+
 
                     <div id="enroll-area">
 
@@ -156,7 +156,6 @@ $pageName = '套裝活動報名'; // 頁面名稱
             </div>
         </div>
     </div>
-</div>
 </div>
 
 
