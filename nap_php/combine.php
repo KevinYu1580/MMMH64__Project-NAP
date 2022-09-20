720828b9d85faa9198426689b93f678184bb1dfd
Shen-Wan Yu <kevin1580@gmail.com>
2022-09-20 14:54:10 +0800

    0920-1454-k

--- nap_php/combine.php
+++ nap_php/combine.php
@@ -8,14 +8,10 @@ $pageName = 'home'; // 頁面名稱
 <link rel="stylesheet" href="./nap_js/bootstrap-5.1.1-dist/css/bootstrap.css">
 <!-- <link rel="stylesheet" href="./nap_js/bootstrap-4.2.1-dist/css/bootstrap.css"> -->
 
-<!-- 加自己的css -->
-    <!-- <link rel="stylesheet" href="./nap_css/component_mobile2.css"> -->
-    
-
-    
 </head>
 <?php include __DIR__. '/parts/navbar.php'; ?>
-
+<!-- 加自己的css -->
+    <!-- <link rel="stylesheet" href="./nap_css/component_mobile2.css"> -->
 
 
 <div class="container">
