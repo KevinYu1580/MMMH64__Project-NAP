6c2dc89f6a68cfa5dbfb4201ab6220b6580e92ab
nora10069 <hweng028@gmail.com>
2022-09-30 18:01:15 +0800

    0930-1801-n

---
+++ nap_php/nap_api/eventlove-del.php
@@ -0,0 +1,19 @@
+<?php
+
+// echo $_SERVER['HTTP_REFERER'];  // 人從哪裡來
+// exit;
+
+require __DIR__. '/../parts/connect_db.php';
+
+if(isset($_GET['sid'])){
+    $sid =  intval($_GET['sid']);
+    $sql = "DELETE FROM likes WHERE likes_sid=$sid";
+    $pdo->query($sql);
+}
+
+$comeFrom = 'love-event.php';
+if(! empty($_SERVER['HTTP_REFERER'])){
+    $comeFrom = $_SERVER['HTTP_REFERER'];
+}
+
+header('Location: '. $comeFrom);
